0617ebf774afb45f9146234f40fbc9e65e87370b
Lulu Cho <lulu.jiyun.cho@gmail.com>
2014-10-29 08:41:03 -0400

    LuluBlue

--- Polynomials/QuadraticR.java
+++ Polynomials/QuadraticR.java
@@ -2,9 +2,10 @@ public class QuadraticR {
 
     public int[] poly = new int[2];
 
-    public QuadraticR (int a; int b; int c;) {
-	
-
+    public QuadraticR (int a, int b, int c) {
+	poly[0] = a;
+	poly[1] = b;
+	poly[2] = c;
     }
 
 }
